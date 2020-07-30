1f0565da0928a57351935137dd498275e17018dc
William Seawright <seawright910@gmail.com>
2020-07-30 18:55:25 +0000

    .gitignore changes to hide api key

---
+++ rideshare/src/main/webapp/config.js
@@ -0,0 +1,7 @@
+
+function config(){
+    document.getElementById("mapAPI").src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARPxINyKUXQkjeJNPxpHceSK9_W_XlHoA&callback=initMap&libraries=places"
+    document.getElementById("mapAPIguest").src="https://maps.googleapis.com/maps/api/js?key=AIzaSyARPxINyKUXQkjeJNPxpHceSK9_W_XlHoA&callback=initMap&libraries=places"
+
+}
+
